47d9c5965007e257c80f5e4c3942d156544a9ab7
Gabriela Michel <gabriela.michel.martinez@gmail.com>
2023-12-31 18:43:03 +0100

    fonctionnement carroussel texte+loop

--- assets/script.js
+++ assets/script.js
@@ -21,14 +21,51 @@ const slides = [
 
 const leftArrow = document.querySelector(".arrow_left");
 const rightArrow = document.querySelector(".arrow_right");
+const dots = document.querySelectorAll(".dot");
+const bannerText = banner.querySelector("p");
+const bannerSlide = document.getElementById("banner");
 
-// Écouteurs d'événements pour les boutons gauche et droite 
+let currentSlide = 0; // Track le slide en cours de visionnage
 
-leftArrow.addEventListener("click", moveLeft);
-rightArrow.addEventListener("click", moveRight);
-function moveLeft(){
-	console.log("tu as cliqué sur la flèche gauche");
+updateActiveDot() // Selectionne le premier bullet point au chargement de la page
+
+
+// Écouteurs d'événements boutons au clic
+
+leftArrow.addEventListener('click', showPreviousSlide);
+rightArrow.addEventListener('click', showNextSlide);
+
+
+/* Fonctions */
+
+function updateActiveDot() {
+	dots.forEach((dot, index) => {
+		dot.classList.remove("dot_selected");
+		if (index === currentSlide) {
+			dot.classList.add("dot_selected");
+		}
+	});
 }
-function moveRight(){
-	console.log("tu as cliqué sur la flèche droite");
+/* "forEach" goes through each bullet point and removes the "dot_selected" class.
+"dot_selected" class is added only to the bullet point corresponding to the current slide. */
+
+// fonction du caroussel mise à jour d'image + textes ne fonction du clic sur le flèches
+
+function showNextSlide() {
+	currentSlide = (currentSlide + 1) % slides.length; 
+	bannerSlide.src = "./assets/images/slideshow/" + slides[currentSlide].image;
+	bannerText.innerHTML = slides[currentSlide].tagLine;
+	updateActiveDot();
+	// vérification fonctionnement écouteur événement flèche
+	console.log ("tu as cliqué sur la flèche suivant");
+
 }
+function showPreviousSlide() {
+	currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+	bannerSlide.src = "./assets/images/slideshow/" + slides[currentSlide].image;
+	bannerText.innerHTML = slides[currentSlide].tagLine;
+	updateActiveDot();
+	// vérification fonctionnement écouteur événement flèche
+	console.log ("tu as cliqué sur la flèche précédent");
+}
+
